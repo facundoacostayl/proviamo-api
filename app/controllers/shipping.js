1ead5aba0057b569bcbda9d230f86b8012a860d1
facundoacostayl@outlook.com <facundoacostayl@outlook.com>
2022-07-28 15:44:01 -0300

    shipping routes updated

--- app/controllers/shipping.js
+++ app/controllers/shipping.js
@@ -3,12 +3,43 @@ const { handleError } = require("../utils/handleError");
 
 const getShippingData = async () => {
     try {
-        const data = await shippingModel.find();
+        const {id} = req.params;
+        const data = await shippingModel.findById(shippingId === parseInt(id));
 
         res.send(data)
     } catch (e) {
-        handleError(res, "ERROR_GET_SHIPPING_DATA", 401)
+        handleError(res, "ERROR_GET_SHIPPING_DATA", 401);
     }
 }
 
-module.exports = {getShippingData}
+const getShippingProducts = async () => {
+    try {
+        const {id} = req.params;
+        const data = await shippingModel.findById(shippingId === parseInt(id));
+
+        res.send(data.order)
+    }catch(e) {
+        handleError(res, "ERROR_GET_SHIPPING_PRODUCTS", 401);
+    }
+}
+
+const getShippingClientData = async () => {
+    try {
+        const {id} = req.params;
+        const data = await shippingModel.findById(shippingId === id);
+
+        res.send(data.shippingData)
+    }catch(e) {
+        handleError(res, "ERROR_GET_SHIPPING_CLIENT_DATA", 401)
+    }
+}
+
+const addShippingData = async () => {
+    try {
+        
+    }catch(e) {
+        handleError(res, "ERROR_POST_SHIPPING_DATA", 401)
+    }
+}
+
+module.exports = {getShippingData, getShippingProducts, getShippingClientData, addShippingData}
--- app/routes/shipping.js
+++ app/routes/shipping.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router()
-const { getShippingData } = require('../controllers/shipping')
+const { getShippingData, getShippingProducts, getShippingClientData, addShippingData } = require('../controllers/shipping')
 
 router.get("/shipping/data", getShippingData)
-router.get("/shipping/products")
-router.post("/shipping")
+router.get("/shipping/products", getShippingProducts)
+router.get("/shipping/client", getShippingClientData)
+router.post("/shipping", addShippingData)
 
 module.exports = router
