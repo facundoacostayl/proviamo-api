71f01bed0caa72418fac9c6289c3eae9420429a2
facundoacostayl@outlook.com <facundoacostayl@outlook.com>
2022-07-29 15:03:19 -0300

    Mercadopago implemented

--- app/controllers/shipping.js
+++ app/controllers/shipping.js
@@ -1,5 +1,9 @@
-const { shippingModel } = require('../models');
+const { shippingModel, productModel } = require('../models');
 const { handleError } = require("../utils/handleError");
+
+/**MERCADO PAGO */
+const mercadopago = require("../../config/mercadopago");
+/*****/
 
 const getShippingData = async () => {
     try {
@@ -36,10 +40,68 @@ const getShippingClientData = async () => {
 
 const addShippingData = async () => {
     try {
-        
+        const {order} = req.body;
+        const ids = order.finalOrder.map(p => p.id);
+        const productsCopy = await productModel.find();
+
+        let preference = {
+            items: [],
+            back_urls: {
+              success: "http://localhost:4000/api/1.0/shipping/feedback",
+              failure: "http://localhost:4000/api/1.0/shipping/feedback",
+              pending: "http://localhost:4000/api/1.0/shipping/feedback",
+            },
+            auto_return: "approved",
+          };
+
+          ids.forEach(id => {
+            const product = productsCopy.find(product => product.id === id);
+
+            preference.items.push({
+                title: product.title,
+                unit_price: product.price,
+                quantity: 1,
+              })
+          })
+
+          const response = await mercadopago.preferences.create(preference);
+          const preferenceId = response.body.id;
+          order.total = order.total;
+          order.date = new Date().toISOString();
+          order.preferenceId = preferenceId;
+          order.status = "Pending";
+          const orders = await shippingModel.find(); //GET ORDER TABLE FROM DB
+          orders.push(order);
+          await shippingModel.findOneAndUpdate(); //ADD ORDER TO DB ORDER TABLE
+          res.send({preferenceId})
+
+
+
     }catch(e) {
         handleError(res, "ERROR_POST_SHIPPING_DATA", 401)
     }
 }
 
-module.exports = {getShippingData, getShippingProducts, getShippingClientData, addShippingData}
+
+    const getFeedback = async(req, res) => {
+        try{
+            const payment = await mercadopago.payment.findById(req.query.payment_id);
+            const merchantOrder = await mercadopago.merchant_orders.findById(payment.body.order.id);
+            const preferenceId = merchantOrder.body.preference_id;
+            const status = payment.body.status;
+            await repository.updateOrderByPreferenceId(preferenceId, status);
+
+            const orders = await shippingModel.find(); //GET ORDER TABLE DATA FROM DB
+            const order = orders.find(o => o.preferenceId === preferenceId);
+            order.status = status;
+            await shippingModel.findOneAndUpdate(); //UPDATE DB ORDER TABLE STATUS
+          
+            res.sendFile(require.resolve("./frontend/index.html"));
+          
+            
+            }catch(e){
+              console.log("ERROR " + e.toString())
+            }
+    }
+
+module.exports = {getShippingData, getShippingProducts, getShippingClientData, addShippingData, getFeedback}
--- app/routes/shipping.js
+++ app/routes/shipping.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const { getShippingData, getShippingProducts, getShippingClientData, addShippingData } = require('../controllers/shipping')
+const { getShippingData, getShippingProducts, getShippingClientData, addShippingData, getFeedback } = require('../controllers/shipping')
 
 router.get("/shipping/data", getShippingData)
 router.get("/shipping/products", getShippingProducts)
 router.get("/shipping/client", getShippingClientData)
 router.post("/shipping", addShippingData)
+router.get("/shipping/feedback", getFeedback)
 
 module.exports = router
